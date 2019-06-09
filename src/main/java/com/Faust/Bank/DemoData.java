d6c45c21e6c0fd02b65dd4e423d438729fc3fb9a
PavelLitvinov97 <p.litvinov7@gmail.com>
2019-06-09 23:28:30 +0300

    Some bullshit fixed

--- src/main/java/com/Faust/Bank/DemoData.java
+++ src/main/java/com/Faust/Bank/DemoData.java
@@ -22,6 +22,7 @@ import java.util.Arrays;
 public class DemoData implements CommandLineRunner {
 
 
+
     @Autowired
     IssueRepository issueRepository;
 
--- src/main/java/com/Faust/Bank/Service/CustomUserDetailsService.java
+++ src/main/java/com/Faust/Bank/Service/CustomUserDetailsService.java
@@ -1,10 +1,13 @@
-package com.Faust.Bank.Security;
+package com.Faust.Bank.Service;
 
 import com.Faust.Bank.Repositories.UserRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -20,4 +23,12 @@ public class CustomUserDetailsService implements UserDetailsService {
         return this.users.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Username: " + username + " not found"));
     }
+    //public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    //    if ("javainuse".equals(username)) {
+    //        return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+    //                new ArrayList<>());
+    //    } else {
+    //        throw new UsernameNotFoundException("User not found with username: " + username);
+    //    }
+    //}
 }
